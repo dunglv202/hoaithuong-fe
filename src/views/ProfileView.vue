bd743f82f38ad4692cc9624b44ecef49f5143e95
dunglv202 <dunglv202@gmail.com>
2024-09-04 19:47:08 +0700

    add loading profile view

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,75 +1,97 @@
 <template>
-    <div class="nav">
-        <el-button class="btn-close" :icon="IconX" circle @click="closeView" />
+  <div class="nav">
+    <el-button class="btn-close" :icon="IconX" circle @click="closeView" />
+  </div>
+  <div class="container">
+    <el-avatar :size="80" :src="profile.avatar">
+      <img src="https://cube.elemecdn.com/e/fd/0fc7d20532fdaf769a25683617711png.png" />
+    </el-avatar>
+    <div>
+      <span class="name">{{ profile.displayName }}</span>
     </div>
-    <div class="container">
-        <el-avatar :size="80" :src="profile.avatar">
-            <img src="https://cube.elemecdn.com/e/fd/0fc7d20532fdaf769a25683617711png.png" />
-        </el-avatar>
-        <div>
-            <span class="name">{{ profile.displayName }}</span>
-        </div>
-        <el-form class="profile-form" :label-position="labelPosition">
-            <el-form-item label="General report URL" label-width="150px">
-                <el-input class="inp" v-model="profile.configs.generalReportUrl" placeholder="Spreadsheet URL" />
-            </el-form-item>
-            <el-form-item label="Sheet name" label-width="150px">
-                <el-input class="inp" v-model="profile.configs.generalReportSheet" placeholder="Sheet name" />
-            </el-form-item>
-            <el-divider border-style="dashed" />
-            <el-form-item label="Detail report URL" label-width="150px">
-                <el-input class="inp" v-model="profile.configs.detailReportUrl" placeholder="Spreadsheet URL" />
-            </el-form-item>
-            <el-form-item label="Sheet name" label-width="150px">
-                <el-input class="inp" v-model="profile.configs.detailReportSheet" placeholder="Sheet name" />
-            </el-form-item>
-            <el-button :loading="submitting" class="btn-submit" type="primary" @click="save">
-                Save
-            </el-button>
-        </el-form>
-    </div>
+    <el-form v-loading="fetching" class="profile-form" :label-position="labelPosition">
+      <el-form-item label="General report URL" label-width="150px">
+        <el-input
+          class="inp"
+          v-model="profile.configs.generalReportUrl"
+          placeholder="Spreadsheet URL"
+        />
+      </el-form-item>
+      <el-form-item label="Sheet name" label-width="150px">
+        <el-input
+          class="inp"
+          v-model="profile.configs.generalReportSheet"
+          placeholder="Sheet name"
+        />
+      </el-form-item>
+      <el-divider border-style="dashed" />
+      <el-form-item label="Detail report URL" label-width="150px">
+        <el-input
+          class="inp"
+          v-model="profile.configs.detailReportUrl"
+          placeholder="Spreadsheet URL"
+        />
+      </el-form-item>
+      <el-form-item label="Sheet name" label-width="150px">
+        <el-input
+          class="inp"
+          v-model="profile.configs.detailReportSheet"
+          placeholder="Sheet name"
+        />
+      </el-form-item>
+      <el-button
+        :loading="submitting"
+        class="btn-submit"
+        type="primary"
+        @click="save"
+        :icon="IconSquareRoundedCheck"
+      >
+        Save
+      </el-button>
+    </el-form>
+  </div>
 </template>
 
 <style scoped>
 .nav {
-    position: sticky;
-    top: 0;
+  position: sticky;
+  top: 0;
 }
 
 .btn-close {
-    float: right;
+  float: right;
 }
 
 .container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 1.5rem;
-    margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.5rem;
+  margin-top: 50px;
 }
 
 .btn-submit {
-    width: 100%;
+  width: 100%;
 }
 
 .inp {
-    width: 300px;
+  width: 300px;
 }
 
 .name {
-    font-size: 1.25rem;
-    font-weight: 500;
+  font-size: 1.25rem;
+  font-weight: 500;
 }
 
 @media screen and (max-width: 768px) {
-    .profile-form {
-        width: 100%;
-        padding: 10px;
-    }
+  .profile-form {
+    width: 100%;
+    padding: 10px;
+  }
 
-    .inp {
-        width: 100%;
-    }
+  .inp {
+    width: 100%;
+  }
 }
 </style>
 
@@ -77,47 +99,53 @@
 import { MOBILE_BREAKPOINT } from '@/configs/layout-config'
 import { type DetailProfile } from '@/models/user'
 import { getDetailProfile, updateDetailProfile } from '@/services/user-service'
-import { IconX } from '@tabler/icons-vue'
+import { IconSquareRoundedCheck, IconX } from '@tabler/icons-vue'
 import { ElMessage } from 'element-plus'
 import { onMounted, ref } from 'vue'
 import { useRouter } from 'vue-router'
 
 const profile = ref<DetailProfile>({
-    displayName: '-',
-    configs: {
-        generalReportUrl: '',
-        generalReportSheet: '',
-        detailReportUrl: '',
-        detailReportSheet: ''
-    }
+  displayName: '-',
+  configs: {
+    generalReportUrl: '',
+    generalReportSheet: '',
+    detailReportUrl: '',
+    detailReportSheet: ''
+  }
 })
 const submitting = ref(false)
 const router = useRouter()
 const labelPosition = ref('left')
+const fetching = ref(false)
 
 const save = async () => {
-    try {
-        submitting.value = true
-        await updateDetailProfile(profile.value)
-        ElMessage({
-            message: 'Saved successfully',
-            type: 'success'
-        })
-    } finally {
-        submitting.value = false
-    }
+  try {
+    submitting.value = true
+    await updateDetailProfile(profile.value)
+    ElMessage({
+      message: 'Saved successfully',
+      type: 'success'
+    })
+  } finally {
+    submitting.value = false
+  }
 }
 
 const closeView = () => {
-    router.back()
+  router.back()
 }
 
 onMounted(async () => {
+  labelPosition.value = window.innerWidth < MOBILE_BREAKPOINT ? 'top' : 'left'
+  try {
+    fetching.value = true
     profile.value = await getDetailProfile()
-    labelPosition.value = (window.innerWidth < MOBILE_BREAKPOINT) ? 'top' : 'left'
+  } finally {
+    fetching.value = false
+  }
 })
 
 window.addEventListener('resize', () => {
-    labelPosition.value = (window.innerWidth < MOBILE_BREAKPOINT) ? 'top' : 'left'
+  labelPosition.value = window.innerWidth < MOBILE_BREAKPOINT ? 'top' : 'left'
 })
 </script>
